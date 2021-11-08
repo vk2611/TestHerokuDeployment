d0879a4577b4fbbf02bd755ec90610c806fe69f7
vishal karhade <sarita34karhade@gmail.com>
2021-11-08 17:11:46 +0530

    controller updated

--- src/main/java/com/example/controller/Controller.java
+++ src/main/java/com/example/controller/Controller.java
@@ -2,6 +2,7 @@ package com.example.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.service.ServiceClass;
@@ -15,5 +16,10 @@ public class Controller {
 	@GetMapping("/")
 	public String getHome() {
 		return service.getHome();
+	}
+
+	@GetMapping("/{id}")
+	public String getHomeId(@RequestBody int id) {
+		return "hello id:-" + id;
 	}
 }
