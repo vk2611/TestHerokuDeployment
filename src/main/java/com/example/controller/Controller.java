70c5acc11f56077b3caa8cb2014823b2b04d5183
vishal karhade <sarita34karhade@gmail.com>
2021-11-08 18:45:38 +0530

    .

--- src/main/java/com/example/controller/Controller.java
+++ src/main/java/com/example/controller/Controller.java
@@ -2,6 +2,7 @@ package com.example.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,7 +19,7 @@ public class Controller {
 		return service.getHome();
 	}
 
-	@GetMapping("/response")
+	@PostMapping("/response")
 	public String getHomeId(@RequestBody String id) {
 		return "hello id:-" + id;
 	}
